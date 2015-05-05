8f9282a4f0441cfa1d3fe64947a3b559e3edd3f0
Svenn-Arne Dragly <s@dragly.com>
2015-05-05 11:23:27 +0200

    Added pull to make_docs

--- doc/make_docs.sh
+++ doc/make_docs.sh
@@ -6,11 +6,14 @@ DOCSDIR=$DIR/../../neuronify-docs
 
 if [ -d $DOCSDIR/.git ]; then
   echo "All good, found neuronify-docs with git repo."
+  echo "Pulling changes in neuronify-docs (note: not pulling in the current folder)."
+  cd $DOCSDIR
+  git pull
 elif [ -d $DOCSDIR ]; then
   echo "Directory $DOCSDIR exists, but is not a git repo. Please delete the directory first."
   exit 1
 else
-  cd $DIR/../../
+  cd $ROOTDIR
   git clone -b gh-pages git@github.com:CINPLA/neuronify.git neuronify-docs
 fi
 
