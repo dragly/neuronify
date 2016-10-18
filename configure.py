f194a2ff681bb60d02ae585eaf0e07b7c751f9b6
Svenn-Arne Dragly <s@dragly.com>
2016-10-18 11:52:45 +0200

    Update configure.py

--- configure.py
+++ configure.py
@@ -2,19 +2,19 @@ from sys import argv
 import subprocess
 import os
 
-if len(argv) < 2:
-    print("Usage: python configure.py <qmake path>")
-    print("qmake path is typically /home/username/apps/Qt/5.6/gcc_64/bin/qmake")
-    exit(0)
+#if len(argv) < 2:
+#    print("Usage: python configure.py <qmake path>")
+#    print("qmake path is typically /home/username/apps/Qt/5.6/gcc_64/bin/qmake")
+#    exit(0)
 
-qmake_path = argv[1]
+#qmake_path = argv[1]
 
 subprocess.call("git submodule init", shell=True)
 subprocess.call("git submodule update", shell=True)
 
-os.mkdir("libs/build-qtcharts")
-os.chdir("libs/build-qtcharts")
+#os.mkdir("libs/build-qtcharts")
+#os.chdir("libs/build-qtcharts")
 
-subprocess.call(qmake_path + " ../qtcharts", shell=True)
-subprocess.call("make -j4", shell=True)
-subprocess.call("make install", shell=True)
+#subprocess.call(qmake_path + " ../qtcharts", shell=True)
+#subprocess.call("make -j4", shell=True)
+#subprocess.call("make install", shell=True)
